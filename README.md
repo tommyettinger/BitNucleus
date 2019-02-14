cbd57b9a4d03a2ebd27ffa15b473464b7e0c8b55
Tommy Ettinger <tommy.ettinger@gmail.com>
2019-02-13 21:05:28 -0800

    Add permitted code from Mindustry, thank you @Anuken

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # BitNucleus
 Pack various small data into one primitive int or long; "Structs for Java"
+
+This brings in code from [Mindustry](https://github.com/Anuken/Mindustry) by Anuken,
+and there are very few changes from the Mindustry code at this point.
+
+## License
+
+The license here is Apache 2.0, with permission granted by Anuken to change the
+license for code using this subset of Mindustry, which is GPL 3.0 when used as a full
+game project.
---
+++ annotations/src/main/java/com/github/tommyettinger/bitnucleus/annotations/Annotations.java
@@ -0,0 +1,24 @@
+package io.anuke.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+public class Annotations{
+
+    /** Marks a class as a special value type struct. Class name must end in 'Struct'.*/
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Struct{
+
+    }
+
+    /**Marks a field of a struct. Optional.*/
+    @Target(ElementType.FIELD)
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface StructField{
+        /**Size of a struct field in bits. Not valid on booleans or floating point numbers.*/
+        int value();
+    }
+}
